778680e7810c0914f7361c5c226b82c0fc88de2d
Keyi Zhang <keyi@stanford.edu>
2020-12-15 18:45:31 -0800

    fix typo and add store breakpoint test

--- tests/test_schema.cc
+++ tests/test_schema.cc
@@ -6,7 +6,7 @@
 
 namespace fs = std::filesystem;
 
-class SchemeTest : public ::testing::Test {
+class SchemaTest : public ::testing::Test {
 protected:
     void SetUp() override {
         auto db_filename = ":memory:";
@@ -18,15 +18,27 @@ protected:
     std::unique_ptr<hgdb::DebugDatabase> db;
 };
 
-TEST_F(SchemeTest, init_db) {  // NOLINT
+TEST_F(SchemaTest, init_db) {  // NOLINT
     ASSERT_NE(db, nullptr);
 }
 
-TEST_F(SchemeTest, store_instance) {  // NOLINT
+TEST_F(SchemaTest, store_instance) {  // NOLINT
     EXPECT_EQ(db->count<hgdb::Instance>(), 0);
     constexpr uint32_t id = 42;
     hgdb::store_instance(*db, id, "top.mod");
     auto result = db->get_pointer<hgdb::Instance>(id);
     EXPECT_TRUE(result);
     EXPECT_EQ(result->id, id);
+}
+
+TEST_F(SchemaTest, store_breakpoint) {  // NOLINT
+    EXPECT_EQ(db->count<hgdb::BreakPoint>(), 0);
+    constexpr uint32_t instance_id = 42;
+    constexpr uint32_t breakpoint_id = 1729;
+    hgdb::store_instance(*db, instance_id, "top.mod");
+    hgdb::store_breakpoint(*db, breakpoint_id, instance_id, __FILE__, __LINE__);
+    auto result = db->get_pointer<hgdb::BreakPoint>(breakpoint_id);
+    EXPECT_TRUE(result);
+    EXPECT_EQ(result->id, breakpoint_id);
+    EXPECT_EQ(result->filename, __FILE__);
 }
