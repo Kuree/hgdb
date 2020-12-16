50028ec9d6cee6446f328ea0385ed04b50d70bed
Keyi Zhang <keyi@stanford.edu>
2020-12-15 18:09:59 -0800

    run tests on ci

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,5 +8,7 @@ ROOT="$(dirname ${ROOT})"
 docker pull keyiz/hgdb:test
 
 docker run -d --name ci-test --rm -it --mount type=bind,source=${ROOT},target=/hgdb keyiz/hgdb:test bash
-# docker exec -i ci-test bash -c "cd /hgdb && pytest tests/ -v"
+# build everything
+docker exec -i ci-test bash -c "cd /hgdb && mkdir build && cd build && cmake .. && make -j"
+docker exec -i ci-test bash -c "cd /hgdb && pytest tests/ build/tests -v"
 docker stop ci-test
