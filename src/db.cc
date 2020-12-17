32a18601ef7cfa93018f520ab2bb6b95f9aa92aa
Keyi Zhang <keyi@stanford.edu>
2020-12-16 17:25:20 -0800

    start to work on debug db client

--- src/db.cc
+++ src/db.cc
@@ -1,1 +1,19 @@
 #include "db.hh"
+
+namespace hgdb {
+
+DebugDatabaseClient::DebugDatabaseClient(const std::string &filename) {
+    db_ = std::make_unique<DebugDatabase>(init_debug_db(filename));
+    db_->sync_schema();
+}
+
+void DebugDatabaseClient::close() {
+    if (!is_closed_) {
+        db_.reset();
+        is_closed_ = true;
+    }
+}
+
+DebugDatabaseClient::~DebugDatabaseClient() { close(); }
+
+}  // namespace hgdb
--- src/db.hh
+++ src/db.hh
@@ -3,4 +3,21 @@
 
 #include "schema.hh"
 
+namespace hgdb {
+/**
+ * Debug database class that handles querying about the design
+ */
+class DebugDatabaseClient {
+public:
+    explicit DebugDatabaseClient(const std::string &filename);
+    void close();
+
+    ~DebugDatabaseClient();
+
+private:
+    std::unique_ptr<DebugDatabase> db_;
+    bool is_closed_ = false;
+};
+}  // namespace hgdb
+
 #endif  // HGDB_DB_HH
