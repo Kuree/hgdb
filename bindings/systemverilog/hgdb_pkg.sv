9cd63ad75aa21b2c348e46c81c7282d4612a8c83
Keyi Zhang <keyi@stanford.edu>
2021-01-01 14:24:45 -0800

    add a systemverilog binding file

---
+++ bindings/systemverilog/hgdb_pkg.sv
@@ -0,0 +1,25 @@
+
+package hgdb_pkg;
+
+// can be called during initial without explicitly setting
+// simulator flags to load VPI entry point;
+// example:
+// in SystemVerilog Test bench
+// initial begin
+
+/*
+* can be called during initial without explicitly setting
+* simulator flags to load VPI entry point;
+* example:
+* in SystemVerilog Test bench
+*   initial begin
+      initialize_hgdb_runtime();
+    end
+  Then in the simulator command line, do
+  xrun -sv_lib libhgdb.so hgdb_pkg.sv [other files]
+  
+  vcs and other simulators share similar command line switches
+*/
+import "DPI-C" function void initialize_hgdb_runtime();
+
+endpackage
