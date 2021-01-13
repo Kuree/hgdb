0c14118a9455a407c448fa52c89eb7e5d026e708
Keyi Zhang <keyi@stanford.edu>
2021-01-13 11:35:33 -0800

    update debug mock server to have multiple top-level instances that share the same definition

--- tests/test_debug_server.cc
+++ tests/test_debug_server.cc
@@ -42,17 +42,19 @@
 auto setup_db_vpi(MockVPIProvider &vpi) {
     using namespace hgdb;
     // create a mock design, see comments above
-    auto db_filename = ":memory:";
+    const auto *db_filename = ":memory:";
     auto db = std::make_unique<DebugDatabase>(init_debug_db(db_filename));
     db->sync_schema();
     // store
     auto variables = {"clk", "rst", "a", "b"};
-    std::vector<std::pair<std::string, std::string>> instance_names = {{"top", "top"},
-                                                                       {"mod", "top.dut"}};
+    // notice that mod and mod2 have the same definition
+    // we do so to test out multiple hierarchy mapping
+    std::vector<std::pair<std::string, std::string>> instance_names = {
+        {"top", "top"}, {"mod", "top.dut"}, {"mod2", "top.dut2"}};
     std::vector<vpiHandle> instance_handles;
-    for (uint32_t instance_id = 0; instance_id < 2; instance_id++) {
+    for (uint32_t instance_id = 0; instance_id < 3; instance_id++) {
         auto const &[def_name, inst_name] = instance_names[instance_id];
-        auto handle = vpi.add_module(def_name, inst_name);
+        auto *handle = vpi.add_module(def_name, inst_name);
         instance_handles.emplace_back(handle);
         if (instance_id == 0) {
             vpi.set_top(handle);
@@ -61,66 +63,68 @@ auto setup_db_vpi(MockVPIProvider &vpi) {
             store_instance(*db, instance_id, def_name);
         }
     }
-    constexpr auto dut_id = 1;
-    // we are only interested in dut
-    std::map<std::string, uint32_t> variable_ids;
-    std::map<std::string, vpiHandle> variable_handles;
-    uint32_t var_id = 0;
-    auto dut_instance_name = instance_names.back().first;
-    auto dut_instance_full_name = instance_names.back().second;
-    auto dut_instance_handle = instance_handles.back();
-    for (const auto &name : variables) {
-        auto var_name = fmt::format("{0}.{1}", dut_instance_name, name);
-        auto var_full_name = fmt::format("{0}.{1}", dut_instance_full_name, name);
-        store_variable(*db, var_id, var_name);
-        variable_ids.emplace(name, var_id);
-        auto handle = vpi.add_signal(dut_instance_handle, var_full_name);
-        variable_handles.emplace(name, handle);
-        // generator variable
-        store_generator_variable(*db, name, dut_id, var_id);
-        var_id++;
-    }
-    // variable that doesn't exist in the symbol table
-    auto temp_variables = {"c", "c_0", "c_1", "c_2", "c_3"};
-    for (auto const &name : temp_variables) {
-        auto var_name = fmt::format("{0}.{1}", dut_instance_name, name);
-        auto var_full_name = fmt::format("{0}.{1}", dut_instance_full_name, name);
-        auto handle = vpi.add_signal(dut_instance_handle, var_full_name);
-        variable_handles.emplace(name, handle);
-        // this is still valid variable, just not showing up in the context/generator map
-        store_variable(*db, var_id, var_name);
-        variable_ids.emplace(name, var_id);
-    }
+    for (auto dut_id = 1; dut_id <= 2; dut_id++) {
+        // we are only interested in dut and dut2
+        std::map<std::string, uint32_t> variable_ids;
+        std::map<std::string, vpiHandle> variable_handles;
+        uint32_t var_id = 0;
+        auto dut_instance_name = instance_names.back().first;
+        auto dut_instance_full_name = instance_names.back().second;
+        auto *dut_instance_handle = instance_handles.back();
+        for (const auto &name : variables) {
+            auto var_name = fmt::format("{0}.{1}", dut_instance_name, name);
+            auto var_full_name = fmt::format("{0}.{1}", dut_instance_full_name, name);
+            store_variable(*db, var_id, var_name);
+            variable_ids.emplace(name, var_id);
+            auto *handle = vpi.add_signal(dut_instance_handle, var_full_name);
+            variable_handles.emplace(name, handle);
+            // generator variable
+            store_generator_variable(*db, name, dut_id, var_id);
+            var_id++;
+        }
+        // variable that doesn't exist in the symbol table
+        auto temp_variables = {"c", "c_0", "c_1", "c_2", "c_3"};
+        for (auto const &name : temp_variables) {
+            auto var_name = fmt::format("{0}.{1}", dut_instance_name, name);
+            auto var_full_name = fmt::format("{0}.{1}", dut_instance_full_name, name);
+            auto *handle = vpi.add_signal(dut_instance_handle, var_full_name);
+            variable_handles[name] = handle;
+            // this is still valid variable, just not showing up in the context/generator map
+            store_variable(*db, var_id, var_name);
+            variable_ids[name] = var_id;
+        }
 
-    // now we need to deal with breakpoints
-    constexpr auto filename = "/tmp/test.py";
-    // assign c_0 = ~a; // a = a  en: a           -> c = ~a
-    store_breakpoint(*db, 0, dut_id, filename, 2, 0, "a");
-    store_context_variable(*db, "a", 0, variable_ids.at("a"));
-    // assign c_1 = 0;  // a = a  en: ~a          -> c = 0
-    store_breakpoint(*db, 1, dut_id, filename, 4, 0, "~a");
-    store_context_variable(*db, "a", 1, variable_ids.at("a"));
-    // assign c_2 = a? c_0: c_1; // a = a en: 1   -> if (a)
-    store_breakpoint(*db, 2, dut_id, filename, 1, 0, "1");
-    store_context_variable(*db, "a", 2, variable_ids.at("a"));
-    // assign c_3 = a; // a = a, c = c_2 en: 1     -> c = a
-    store_breakpoint(*db, 3, dut_id, filename, 5, 0, "1");
-    store_context_variable(*db, "a", 3, variable_ids.at("a"));
-    store_context_variable(*db, "c", 3, variable_ids.at("c_2"));
+        // now we need to deal with breakpoints
+        constexpr auto filename = "/tmp/test.py";
+        // assign c_0 = ~a; // a = a  en: a           -> c = ~a
+        store_breakpoint(*db, 0 + dut_id * 5, dut_id, filename, 2, 0, "a");
+        store_context_variable(*db, "a", 0 + dut_id * 5, variable_ids.at("a"));
+        // assign c_1 = 0;  // a = a  en: ~a          -> c = 0
+        store_breakpoint(*db, 1 + dut_id * 5, dut_id, filename, 4, 0, "~a");
+        store_context_variable(*db, "a", 1 + dut_id * 5, variable_ids.at("a"));
+        // assign c_2 = a? c_0: c_1; // a = a en: 1   -> if (a)
+        store_breakpoint(*db, 2 + dut_id * 5, dut_id, filename, 1, 0, "1");
+        store_context_variable(*db, "a", 2 + dut_id * 5, variable_ids.at("a"));
+        // assign c_3 = a; // a = a, c = c_2 en: 1     -> c = a
+        store_breakpoint(*db, 3 + dut_id * 5, dut_id, filename, 5, 0, "1");
+        store_context_variable(*db, "a", 3 + dut_id * 5, variable_ids.at("a"));
+        store_context_variable(*db, "c", 3 + dut_id * 5, variable_ids.at("c_2"));
 
-    // set values
-    // we simulate the case where a is 1
-    // in this case we have
-    // c_0 = ~a -> 1
-    // c_1 = 0
-    // c_2 = 0
-    // c_3 = 1
-    // notice that we don't have b's value yet, so when we query we will have ERROR as the result
-    vpi.set_signal_value(variable_handles.at("c_0"), 1);
-    vpi.set_signal_value(variable_handles.at("c_1"), 0);
-    vpi.set_signal_value(variable_handles.at("c_2"), 0);
-    vpi.set_signal_value(variable_handles.at("c_3"), 1);
-    vpi.set_signal_value(variable_handles.at("a"), 1);
+        // set values
+        // we simulate the case where a is 1
+        // in this case we have
+        // c_0 = ~a -> 1
+        // c_1 = 0
+        // c_2 = 0
+        // c_3 = 1
+        // notice that we don't have b's value yet, so when we query we will have ERROR as the
+        // result
+        vpi.set_signal_value(variable_handles.at("c_0"), 1);
+        vpi.set_signal_value(variable_handles.at("c_1"), 0);
+        vpi.set_signal_value(variable_handles.at("c_2"), 0);
+        vpi.set_signal_value(variable_handles.at("c_3"), 1);
+        vpi.set_signal_value(variable_handles.at("a"), 1);
+    }
 
     auto db_client = std::make_unique<hgdb::DebugDatabaseClient>(std::move(db));
     return db_client;
