2ae421920e9fe3898c0d3147ec3331d1caaebda4
Keyi Zhang <keyi@stanford.edu>
2021-01-21 10:42:32 -0800

    add deployment script for libhgdb

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xe
+
+# currently only supports linux so far. MacOS build is just so much pain
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ROOT=$(dirname "${DIR}")
+docker run -it -d --rm --name manylinux -v "${ROOT}":/hgdb keyiz/manylinux bash
+docker exec -i manylinux bash -c 'cd /hgdb && python setup.py bdist_wheel'
+docker exec -i manylinux bash -c 'cd /hgdb && auditwheel repair dist/* -w wheels'
+# use the fix wheel script
+docker exec -i manylinux bash -c 'pip install wheeltools'
+docker exec -i manylinux bash -c 'cd /hgdb && python scripts/fix_wheel.py wheels/*.whl -w wheelhouse'
+docker stop manylinux
---
+++ setup.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+# Notice:
+# Much of the content is copied from
+# https://github.com/pybind/cmake_example/blob/master/setup.py
+# with the following changes:
+#  - On unix, make is used instead of Ninja.
+#  - use DEBUG=1 to build in debug mode
+
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cfg = "Debug" if self.debug else "Release"
+        if "DEBUG" in os.environ:
+            cfg = "Debug"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+        ]
+        build_args = []
+
+        if self.compiler.compiler_type == "msvc":
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+                ]
+                build_args += ["--config", cfg]
+
+        build_args += ["-j2"]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", ".", "--target", "hgdb"] + build_args, cwd=self.build_temp
+        )
+
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root_dir, "README.rst")) as f:
+    long_description = f.read()
+with open(os.path.join(root_dir, "VERSION")) as f:
+    version = f.read().strip()
+
+setup(
+    name='libhgdb',
+    version=version,
+    author='Keyi Zhang',
+    author_email='keyi@cs.stanford.edu',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/Kuree/hgdb",
+    python_requires=">=3.6",
+    ext_modules=[CMakeExtension("libhgdb")],
+    cmdclass={"build_ext": CMakeBuild},
+)
