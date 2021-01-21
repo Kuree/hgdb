535c5439257cf7d047e0d9f8cd07d27efa8839c6
Keyi Zhang <keyi@stanford.edu>
2021-01-21 11:25:27 -0800

    adjust build and deployment code, especially for macos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,5 @@ docker exec -i manylinux bash -c 'cd /hgdb && python setup.py bdist_wheel'
 docker exec -i manylinux bash -c 'cd /hgdb && auditwheel repair dist/* -w wheels'
 # use the fix wheel script
 docker exec -i manylinux bash -c 'pip install wheeltools'
-docker exec -i manylinux bash -c 'cd /hgdb && python scripts/fix_wheel.py wheels/*.whl -w wheelhouse'
+docker exec -i manylinux bash -c 'cd /hgdb && mkdir -p wheelhouse && python scripts/fix_wheel.py wheels/*.whl -w wheelhouse'
 docker stop manylinux
