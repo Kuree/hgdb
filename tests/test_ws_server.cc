bb28979d2b823450b3454964c0cb9aee06e9a409
Keyi Zhang <keyi@stanford.edu>
2021-01-22 22:44:41 -0800

    add topic publish test

--- tests/test_ws_server.cc
+++ tests/test_ws_server.cc
@@ -1,11 +1,12 @@
+#include <array>
 #include <chrono>
 #include <iostream>
 #include <thread>
-#include <array>
 
 #include "../src/server.hh"
 
 constexpr auto stop_msg = "stop";
+constexpr auto topic_msg = "42";
 // for some reason gcc-10 and clang-11 still can't support constexpr std::string
 // specified in C++20
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p0980r1.pdf
@@ -27,8 +28,12 @@ int main(int argc, char* argv[]) {
     // need to defer the shutdown using a thread
     std::thread t;
     // make it an echo server
-    auto echo = [&server, &t](const std::string& msg, uint64_t) {
+    auto echo = [&server, &t](const std::string& msg, uint64_t conn_id) {
+        // this is broadcast
         server.send(msg);
+
+        // publish to modified message to a particular topic
+        server.send(msg + msg, topic_msg);
         // if the message is stop, we stop the server
         if (msg == stop_msg) {
             printf("shutting down\n");
@@ -38,6 +43,9 @@ int main(int argc, char* argv[]) {
                 std::this_thread::sleep_for(0.5s);
                 server.stop();
             });
+        } else if (msg == topic_msg) {
+            // add it to the topic
+            server.add_to_topic(topic_msg, conn_id);
         }
     };
     server.set_on_message(echo);
--- tests/test_ws_server.py
+++ tests/test_ws_server.py
@@ -50,10 +50,35 @@ def test_shutdown(start_server, find_free_port):
     assert killed
 
 
+def test_topic_pub(start_server, find_free_port):
+    port = find_free_port()
+    s = start_server(port, "test_ws_server", wait=0.05)
+
+    async def send_msg():
+        uri = "ws://localhost:{0}".format(port)
+        payload1 = "42"
+        payload2 = "hello world"
+        async with websockets.connect(uri) as ws1:
+            async with websockets.connect(uri) as ws2:
+                await ws1.send(payload1)
+                echo1 = await ws1.recv()
+                assert echo1 == payload1
+                await ws2.send(payload2)
+                await ws1.recv()
+                echo2 = await ws1.recv()
+                assert(echo2 == payload2 * 2)
+
+    asyncio.get_event_loop().run_until_complete(send_msg())
+    # kill the server
+    s.terminate()
+    while s.poll() is None:
+        pass
+
+
 if __name__ == "__main__":
     import os
     import sys
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     from conftest import start_server_fn, find_free_port_fn
 
-    test_shutdown(start_server_fn, find_free_port_fn)
+    test_topic_pub(start_server_fn, find_free_port_fn)
