64def1e166c1ae40c1a26ecf7ab1c01a327f4241
Keyi Zhang <keyi@stanford.edu>
2021-02-01 17:48:51 -0800

    add replay exe implementation; need to add tests

--- tools/hgdb-replay/hgdb-replay.cc
+++ tools/hgdb-replay/hgdb-replay.cc
@@ -1,3 +1,27 @@
-int main() {
+#include "engine.hh"
+#include "sim.hh"
+#include <iostream>
 
+
+void print_usage(const std::string &program_name) {
+    std::cerr << "Usage: " << program_name << " waveform.vcd [args]" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+    std::string filename = argv[1];
+    auto db = std::make_unique<hgdb::vcd::VCDDatabase>(filename);
+    auto vpi = std::make_unique<hgdb::replay::ReplayVPIProvider>(std::move(db));
+    // set argv
+    vpi->set_argv(argc, argv);
+
+    hgdb::replay::EmulationEngine engine(vpi.get());
+
+    // set up the debug runtime
+    hgdb::initialize_hgdb_runtime_vpi(std::move(vpi), true);
+
+    engine.run();
 }
