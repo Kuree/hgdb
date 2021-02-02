58af94a8ded3cb27b505914149c89c83aba01bb4
Keyi Zhang <keyi@stanford.edu>
2021-02-02 00:16:51 -0800

    adding error check and logging for replay tool

--- tools/hgdb-replay/hgdb-replay.cc
+++ tools/hgdb-replay/hgdb-replay.cc
@@ -2,6 +2,8 @@
 
 #include "engine.hh"
 #include "sim.hh"
+#include "log.hh"
+#include <filesystem>
 
 void print_usage(const std::string &program_name) {
     std::cerr << "Usage: " << program_name << " waveform.vcd [args]" << std::endl;
@@ -12,16 +14,19 @@ int main(int argc, char *argv[]) {
         print_usage(argv[0]);
         return EXIT_FAILURE;
     }
+    namespace log = hgdb::log;
+    log::log(log::log_level::info, "Building VCD database...");
+
     std::string filename = argv[1];
+    if (!std::filesystem::exists(filename)) {
+        std::cerr << "Unable to find " << filename << std::endl;
+        return EXIT_FAILURE;
+    }
+
     auto db = std::make_unique<hgdb::vcd::VCDDatabase>(filename);
+
     // notice that db will lose ownership soon. use raw pointer instead
     auto *db_ptr = db.get();
-    // compute the mapping
-    auto mapping_func = [db_ptr](const std::unordered_set<std::string> &instance_names)
-        -> std::unordered_map<std::string, std::string> {
-        auto mapping = db_ptr->compute_instance_mapping(instance_names);
-        return {{mapping.first, mapping.second}};
-    };
 
     auto vpi = std::make_unique<hgdb::replay::ReplayVPIProvider>(std::move(db));
     // set argv
@@ -30,9 +35,19 @@ int main(int argc, char *argv[]) {
     hgdb::replay::EmulationEngine engine(vpi.get());
 
     // set up the debug runtime
+    log::log(log::log_level::info, "Initializing HGDB runtime...");
     auto *debugger = hgdb::initialize_hgdb_runtime_vpi(std::move(vpi), false);
     // set the custom compute function
+
+    // compute the mapping
+    auto mapping_func = [db_ptr](const std::unordered_set<std::string> &instance_names)
+        -> std::unordered_map<std::string, std::string> {
+      auto mapping = db_ptr->compute_instance_mapping(instance_names);
+      return {{mapping.first, mapping.second}};
+    };
+    log::log(log::log_level::info, "Calculating design hierarchy...");
     debugger->rtl_client()->set_custom_hierarchy_func(mapping_func);
 
+    log::log(log::log_level::info, "Starting HGDB replay...");
     engine.run();
 }
