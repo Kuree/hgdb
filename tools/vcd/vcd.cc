2cf94f20499e616d4154f7f0bad9659b9c26ed5e
Keyi Zhang <keyi@stanford.edu>
2021-02-09 14:30:04 -0800

    refactor out vcd parsing logic

---
+++ tools/vcd/vcd.cc
@@ -0,0 +1,237 @@
+#include "vcd.hh"
+
+#include <sstream>
+
+namespace hgdb::vcd {
+
+enum class sv { Date, Version, Timescale, Comment, Scope, Upscope, Var, Enddefinitions };
+
+constexpr auto *end_str = "$end";
+
+VCDParser::VCDParser(const std::string &filename) : filename_(filename) {
+    stream_ = std::ifstream(filename);
+    if (stream_.bad()) {
+        error_message_ = "Unable to open " + filename_;
+    }
+}
+
+std::string next_token(std::istream &stream) {
+    std::stringstream result;
+    uint64_t length = 0;
+    while (!stream.eof()) {
+        char c;
+        stream.get(c);
+        if (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\f') {
+            if (length == 0)
+                continue;
+            else
+                break;
+        }
+        result << c;
+        length++;
+    }
+
+    return result.str();
+}
+
+bool VCDParser::parse() {
+    if (stream_.bad()) return false;
+    // the code below is mostly designed by Teguh Hofstee (https://github.com/hofstee)
+    // heavily refactored to fit into hgdb's needs
+
+    static const std::unordered_map<std::basic_string_view<char>, sv> sv_map = {
+        {"$date", sv::Date},
+        {"$version", sv::Version},
+        {"$timescale", sv::Timescale},
+        {"$comment", sv::Comment},
+        {"$scope", sv::Scope},
+        {"$upscope", sv::Upscope},
+        {"$var", sv::Var},
+        {"$enddefinitions", sv::Enddefinitions}};
+    static const std::unordered_map<sv, VCDMetaInfo::MetaType> meta_map = {
+        {sv::Date, VCDMetaInfo::MetaType::date},
+        {sv::Version, VCDMetaInfo::MetaType::version},
+        {sv::Timescale, VCDMetaInfo::MetaType::timescale},
+        {sv::Comment, VCDMetaInfo::MetaType::comment}};
+
+    while (true) {
+        auto token = next_token(stream_);
+        if (token.empty()) return true;
+
+        if (sv_map.find(token) == sv_map.end()) {
+            error_message_ = "Unable to find token: " + token;
+            return false;
+        }
+
+        switch (sv_map.at(token)) {
+            case sv::Date:
+            case sv::Version:
+            case sv::Timescale:
+            case sv::Comment: {
+                std::stringstream ss;
+                while ((token = next_token(stream_)) != end_str) {
+                    ss << " " << token;
+                }
+                if (on_meta_info_) {
+                    VCDMetaInfo meta_info{.type = meta_map.at(sv_map.at(token)), .value = ss.str()};
+                    (*on_meta_info_)(meta_info);
+                }
+                break;
+            }
+            case sv::Scope: {
+                if (!parse_scope_def()) return false;
+                break;
+            }
+            case sv::Upscope: {
+                token = next_token(stream_);
+                if (!check_end(token)) return false;
+                if (on_exit_scope_) (*on_exit_scope_)();
+                break;
+            }
+            case sv::Var: {
+                if (!parse_var_def()) return false;
+                break;
+            }
+            case sv::Enddefinitions: {
+                token = next_token(stream_);
+                if (!check_end(token)) return false;
+                if (!parse_vcd_values()) return false;
+                break;
+            }
+            default: {
+            }
+        }
+    }
+
+    return true;
+}
+
+void VCDParser::set_exit_scope(const std::optional<std::function<void()>> &func) {
+    on_exit_scope_ = func;
+}
+
+void VCDParser::set_on_enter_scope(
+    const std::optional<std::function<void(const VCDScopeDef &)>> &func) {
+    on_enter_scope_ = func;
+}
+
+void VCDParser::set_on_meta_info(
+    const std::optional<std::function<void(const VCDMetaInfo &)>> &func) {
+    on_meta_info_ = func;
+}
+
+void VCDParser::set_value_change(const std::optional<std::function<void(const VCDValue &)>> &func) {
+    on_value_change_ = func;
+}
+
+void VCDParser::set_on_var_def(const std::optional<std::function<void(const VCDVarDef &)>> &func) {
+    on_var_def_ = func;
+}
+
+VCDParser::~VCDParser() {
+    // close the stream
+    if (!stream_.bad()) {
+        stream_.close();
+    }
+}
+
+bool VCDParser::parse_scope_def() {
+    static VCDScopeDef scope_def;
+    scope_def.type = next_token(stream_);  // scope type
+    scope_def.name = next_token(stream_);
+    auto token = next_token(stream_);
+    if (token != end_str) {
+        error_message_ = "Illegal VCD file";
+        return false;
+    }
+    if (on_enter_scope_) {
+        (*on_enter_scope_)(scope_def);
+    }
+    return true;
+}
+
+bool VCDParser::parse_var_def() {
+    static VCDVarDef var_def;
+
+    var_def.type = next_token(stream_);
+    var_def.width = std::stoul(next_token(stream_));
+    var_def.identifier = next_token(stream_);
+    var_def.name = next_token(stream_);
+
+    auto token = next_token(stream_);
+    if (token != end_str) {
+        // slice
+        var_def.slice = token;
+        token = next_token(stream_);
+        if (token != end_str) {
+            return false;
+        }
+    } else {
+        var_def.slice = "";
+    }
+
+    if (on_var_def_) {
+        (*on_var_def_)(var_def);
+    }
+    return true;
+}
+
+bool VCDParser::parse_vcd_values() {
+    size_t timestamp = 0;
+
+    auto add_value = [this, &timestamp](const std::string &identifier, const std::string &value) {
+        VCDValue v{.time = timestamp, .identifier = identifier, .value = value};
+        if (on_value_change_) {
+            (*on_value_change_)(v);
+        }
+    };
+
+    while (true) {
+        auto token = next_token(stream_);
+        if (token.empty()) return true;
+
+        if (token[0] == '#') {
+            timestamp = std::stoi(std::string(token.substr(1)));
+        } else if (std::string("01xz").find(token[0]) != std::string::npos) {
+            auto value = std::string(1, token[0]);
+            auto ident = std::string(token.substr(1));
+
+        } else if (std::string("b").find(token[0]) != std::string::npos) {
+            auto value = std::string(token.substr(1));
+            auto ident = std::string(next_token(stream_));
+
+            add_value(ident, value);
+        } else if (token == "$dumpvars" || token == "$dumpall" || token == "$dumpon" ||
+                   token == "$dumpoff") {
+            while (true) {
+                token = next_token(stream_);
+                if (token == "$end") {
+                    break;
+                } else if (std::string("01xz").find(token[0]) != std::string::npos) {
+                    auto value = std::string(1, token[0]);
+                    auto ident = std::string(token.substr(1));
+
+                    add_value(ident, value);
+                } else if (std::string("b").find(token[0]) != std::string::npos) {
+                    auto value = std::string(token.substr(1));
+                    auto ident = std::string(next_token(stream_));
+
+                    add_value(ident, value);
+                }
+            }
+        }
+    }
+}
+
+bool VCDParser::check_end(const std::string &token) {
+    if (token != end_str) {
+        std::stringstream ss;
+        ss << "Illegal VCD file " << filename_ << std::endl;
+        ss << "  Missing " << end_str;
+        error_message_ = ss.str();
+        return false;
+    }
+    return true;
+}
+
+}  // namespace hgdb::vcd
---
+++ tools/vcd/vcd.hh
@@ -0,0 +1,77 @@
+#ifndef HGDB_TOOLS_VCD_HH
+#define HGDB_TOOLS_VCD_HH
+
+#include <fstream>
+#include <functional>
+#include <optional>
+#include <stack>
+#include <string>
+#include <unordered_set>
+
+namespace hgdb::vcd {
+
+struct VCDScopeDef {
+    std::string name;
+    std::string type;
+};
+
+struct VCDVarDef {
+    std::string identifier;
+    std::string type;
+    uint32_t width;
+    std::string name;
+    std::string slice;
+};
+
+struct VCDValue {
+    uint64_t time;
+    std::string identifier;
+    std::string value;
+};
+
+struct VCDMetaInfo {
+    enum class MetaType { date, version, timescale, comment };
+    MetaType type;
+    std::string value;
+};
+
+class VCDParser {
+public:
+    explicit VCDParser(const std::string &filename);
+    bool parse();
+
+    // set callback
+    void set_on_meta_info(const std::optional<std::function<void(const VCDMetaInfo &)>> &func);
+    void set_on_enter_scope(const std::optional<std::function<void(const VCDScopeDef &)>> &func);
+    void set_exit_scope(const std::optional<std::function<void()>> &func);
+    void set_value_change(const std::optional<std::function<void(const VCDValue &)>> &func);
+    void set_on_var_def(const std::optional<std::function<void(const VCDVarDef &)>> &func);
+
+    const std::string &error_message() const { return error_message_; }
+
+    ~VCDParser();
+
+private:
+    std::ifstream stream_;
+    std::string filename_;
+
+    // callbacks
+    std::optional<std::function<void(const VCDMetaInfo &)>> on_meta_info_;
+    std::optional<std::function<void(const VCDScopeDef &)>> on_enter_scope_;
+    std::optional<std::function<void()>> on_exit_scope_;
+    std::optional<std::function<void(const VCDValue &)>> on_value_change_;
+    std::optional<std::function<void(const VCDVarDef &)>> on_var_def_;
+
+    std::string error_message_;
+
+    // parser helpers
+    bool parse_scope_def();
+    bool parse_var_def();
+    bool parse_vcd_values();
+
+    bool check_end(const std::string &token);
+};
+
+}  // namespace hgdb::vcd
+
+#endif  // HGDB_TOOLS_VCD_HH
