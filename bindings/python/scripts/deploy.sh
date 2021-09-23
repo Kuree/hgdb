926a2b5e955e7bbf22629f5f70460d04d0c23eb5
Keyi Zhang <keyi@stanford.edu>
2021-09-23 16:53:16 -0700

    add python3.9 support

--- bindings/python/scripts/deploy.sh
+++ bindings/python/scripts/deploy.sh
@@ -5,6 +5,6 @@ ROOT="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 ROOT="$(dirname "$(dirname "$(dirname "${ROOT}")")")"
 docker run -it --rm -d --name manylinux -v "${ROOT}":/hgdb keyiz/manylinux bash
 # python changes its python tag...
-docker exec -i manylinux bash -c 'cd /hgdb/bindings/python && for PYBIN in cp36-cp36m cp37-cp37m cp38-cp38; do /opt/python/${PYBIN}/bin/python setup.py bdist_wheel; done'
+docker exec -i manylinux bash -c 'cd /hgdb/bindings/python && for PYBIN in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39; do /opt/python/${PYBIN}/bin/python setup.py bdist_wheel; done'
 docker exec -i manylinux bash -c 'cd /hgdb/bindings/python&& for WHEEL in dist/*.whl; do auditwheel repair "${WHEEL}"; done'
 docker stop manylinux
