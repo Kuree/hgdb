cfd77526f4bb9969616193ea8fc20bda33d97c90
Keyi Zhang <keyi@stanford.edu>
2021-10-14 17:25:28 -0700

    maybe a little bit delay will help?

--- tests/test_debug_server.py
+++ tests/test_debug_server.py
@@ -387,6 +387,7 @@ def test_debug_env_value(start_server, find_free_port):
     async def test_logic():
         async with hgdb.HGDBClient(uri, None) as client:
             await client.connect()
+            time.sleep(0.5)
             for i in range(5):
                 await client.continue_()
                 bp = await client.recv_bp()
