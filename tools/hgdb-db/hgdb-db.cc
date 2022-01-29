b9c733cee40f5592853cc0d4e0ecdfca97dd4824
Keyi Zhang <keyi@stanford.edu>
2022-01-28 20:48:05 -0800

    make the hgdb-db tool into a interactive app instead

--- tools/hgdb-db/hgdb-db.cc
+++ tools/hgdb-db/hgdb-db.cc
@@ -1,41 +1,104 @@
 #include <filesystem>
 
 #include "../src/db.hh"
-#include "argparse/argparse.hpp"
+#include "cli/cli.h"
+#include "cli/clilocalsession.h"
+#include "cli/standaloneasioscheduler.h"
 #include "fmt/format.h"
 
 #define STRINGIFY2(X) #X
 #define STRINGIFY(X) STRINGIFY2(X)
 #define VERSION_STR STRINGIFY(VERSION_NUMBER)
 
-std::optional<std::string> parse_main_arg(const std::vector<std::string> &args) {
-    if (args.empty()) return std::nullopt;
-    auto program_name = std::filesystem::path(args[0]).filename();
-    argparse::ArgumentParser parser(program_name.string(), VERSION_STR);
-    parser.add_description(fmt::format("HGDB symbol table tool"));
-    parser.add_argument("file").help("symbol table URI");
-    std::string path;
-    try {
-        parser.parse_args(args);
-        path = parser.get<std::string>("file");
-    } catch (const std::runtime_error &error) {
-        std::cerr << error.what() << std::endl;
-        return std::nullopt;
+using MainScheduler = cli::StandaloneAsioScheduler;
+
+std::string get_program_name(char **argv) {
+    std::string program_name;
+    if (std::getenv("HGDB_PYTHON_PACKAGE")) {
+        auto path = std::filesystem::path(program_name);
+        program_name = path.filename();
+    } else {
+        program_name = argv[0];
     }
-    return path;
+    return program_name;
+}
+
+bool check_db(std::unique_ptr<hgdb::SymbolTableProvider> &db, std::ostream &os) {
+    if (!db) {
+        cli::SetColor();
+        os << "Symbol table not loaded" << std::endl;
+        cli::SetNoColor();
+    }
+    return db != nullptr;
+}
+
+template <typename T, typename K = void>
+void render(T value, std::ostream &os) {
+    if constexpr (std::is_same<std::string, T>::value || std::is_arithmetic<T>::value) {
+        os << value << std::endl;
+    } else if constexpr (std::is_same<std::vector<K>, T>::value) {
+        auto max_width = std::to_string(value.size() - 1).size();
+        auto fmt_str = fmt::format("[{{0:{0}}}]: {{1}}", max_width);
+        for (auto i = 0u; i < value.size(); i++) {
+            os << fmt::format(fmt_str.data(), i, value[i]) << std::endl;
+        }
+    }
+}
+
+auto get_instance(cli::Menu &menu, std::unique_ptr<hgdb::SymbolTableProvider> &db) {
+    auto sub_menu = std::make_unique<cli::Menu>("instance", "Instance data query");
+    sub_menu->Insert("id", [&db](std::ostream &os, uint32_t id) {
+        if (!check_db(db, os)) return;
+        auto instance = db->get_instance_name(id);
+        if (instance) {
+            render(*instance, os);
+        }
+    });
+    sub_menu->Insert("bp-id", [&db](std::ostream &os, uint32_t id) {
+        if (!check_db(db, os)) return;
+        auto instance = db->get_instance_name_from_bp(id);
+        if (instance) {
+            render(*instance, os);
+        }
+    });
+    sub_menu->Insert("list", [&db](std::ostream &os) {
+        if (!check_db(db, os)) return;
+        auto instances = db->get_instance_names();
+        render<decltype(instances), std::string>(instances, os);
+    });
+    return std::move(sub_menu);
 }
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> args(argc);
-    for (int i = 0; i < argc; i++) args[i] = argv[i];
+    auto program_name = get_program_name(argv);
+    auto root_menu =
+        std::make_unique<cli::Menu>("hgdb", fmt::format("{0} v{1}", program_name, VERSION_STR));
+    std::unique_ptr<hgdb::SymbolTableProvider> db;
 
-    auto filename = parse_main_arg(args);
-    if (!filename) return EXIT_FAILURE;
-
-    auto db = hgdb::create_symbol_table(*filename);
-    if (!db || db->bad()) {
-        return EXIT_FAILURE;
+    // preload the symbol table
+    if (argc > 1) {
+        db = hgdb::create_symbol_table(argv[1]);
     }
 
+    root_menu->Insert("load",
+                      [&db](std::ostream &, const std::string &filename) {
+                          db = hgdb::create_symbol_table(filename);
+                      },
+                      "Load symbol table", {"path/uri"});
+
+    root_menu->Insert(get_instance(*root_menu, db));
+
+    cli::Cli cli(std::move(root_menu));
+    MainScheduler scheduler;
+    cli::CliLocalTerminalSession session(cli, scheduler, std::cout, 200);
+    session.ExitAction([&db, &scheduler](std::ostream &) {
+        scheduler.Stop();
+        if (db) db.reset();
+    });
+
+    scheduler.Run();
+
+    // print out new line to avoid making the prompt the same line
+    std::cout << std::endl;
     return EXIT_SUCCESS;
 }
