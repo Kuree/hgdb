dd202a9ba012e7e028ad1073240cfd2fe951e8dd
Keyi Zhang <keyi@stanford.edu>
2022-07-31 15:47:24 -0700

    add json db compression logic; add more tests

--- include/json.hh
+++ include/json.hh
@@ -1,9 +1,15 @@
 #ifndef HGDB_JSON_HH
 #define HGDB_JSON_HH
 
+#include <functional>
 #include <memory>
+#include <optional>
 #include <sstream>
+#include <string>
 #include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
 #include <vector>
 
 namespace hgdb::json {
@@ -77,17 +83,25 @@ private:
 
 class Module;
 class VarStmt;
+class VarStmt;
+class SymbolTable;
 
 class ScopeBase {
 public:
     virtual void serialize(JSONWriter &w) const = 0;
     virtual ~ScopeBase() = default;
+
+    ScopeBase *parent = nullptr;
+
+    [[nodiscard]] virtual const std::string &get_filename() const = 0;
 };
 
 template <typename C = std::nullptr_t>
 class Scope : public ScopeBase {
 public:
     Scope() = default;
+    explicit Scope(uint32_t line) : line_num(line) {}
+
     template <typename T, typename... Args>
     T *create_scope(Args &&...args) {
         auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
@@ -100,8 +114,8 @@ public:
 
     void serialize(JSONWriter &w) const override {
         w.begin_obj().key("type").value(type());
-        if (!filename_.empty()) {
-            w.key("filename").value(filename_);
+        if (!filename.empty()) {
+            w.key("filename").value(filename);
         }
         if (line_num != 0) {
             w.key("line").value(line_num);
@@ -109,11 +123,19 @@ public:
                 w.key("column").value(column_num);
             }
         }
-        w.key("scope").begin_array();
-        for (auto const &scope : scopes_) {
-            scope->serialize(w);
+
+        bool has_scope = !scopes_.empty();
+        if constexpr (std::is_same_v<C, Module>) {
+            has_scope = true;
         }
-        w.end_array();
+
+        if (has_scope) {
+            w.key("scope").begin_array();
+            for (auto const &scope : scopes_) {
+                scope->serialize(w);
+            }
+            w.end_array();
+        }
 
         serialize_(w);
 
@@ -123,7 +145,15 @@ public:
     [[nodiscard]] auto begin() const { return scopes_.begin(); }
     [[nodiscard]] auto end() const { return scopes_.end(); }
 
-    std::string filename_;
+    [[nodiscard]] const std::string &get_filename() const override {
+        if (!filename.empty()) {
+            return filename;
+        }
+        if (parent) return parent->get_filename();
+        return filename;
+    }
+
+    std::string filename;
     uint32_t line_num = 0;
     uint32_t column_num = 0;
 
@@ -138,6 +168,7 @@ private:
         // make sure the scope creation is valid
         static_assert(!std::is_same_v<T, Module> && !std::is_same_v<C, VarStmt>, "Invalid scope");
         auto *res = ptr.get();
+        res->parent = this;
         scopes_.emplace_back(std::move(ptr));
         return res;
     }
@@ -150,32 +181,52 @@ struct Variable {
     std::string value;
     bool rtl = false;
 
+    std::optional<uint64_t> id;
+
     void serialize(JSONWriter &w) const {
-        w.begin_obj()
-            .key("name")
-            .value(name)
-            .key("value")
-            .value(value)
-            .key("rtl")
-            .value(rtl)
-            .end_obj();
+        w.begin_obj().key("name").value(name).key("value").value(value).key("rtl").value(rtl);
+        if (id) {
+            w.key("id").value(std::to_string(*id));
+        }
+        w.end_obj();
     }
+
+    bool operator==(const Variable &v) const {
+        return name == v.name && value == v.value && rtl == v.rtl;
+    }
+
+    struct hash_fn {
+        std::size_t operator()(const Variable &v) const {
+            auto hash = std::hash<std::string>{};
+            auto h1 = hash(v.name);
+            auto h2 = hash(v.value);
+            auto h3 = std::hash<bool>{}(v.rtl);
+            return h1 ^ h2 ^ h3;
+        }
+    };
 };
 
 class VarStmt : public Scope<VarStmt> {
 public:
-    VarStmt(Variable var, bool is_decl) : var_(std::move(var)), is_decl_(is_decl) {}
+    VarStmt(Variable var, uint64_t line, bool is_decl)
+        : Scope<VarStmt>(line), var_(std::move(var)), is_decl_(is_decl) {}
 
     [[nodiscard]] std::string_view type() const override { return is_decl_ ? "decl" : "assign"; }
+
+    friend class SymbolTable;
 
 protected:
     void serialize_(JSONWriter &w) const override {
         w.key("variable");
-        var_.serialize(w);
+        if (var_.index() == 0) {
+            std::get<0>(var_).serialize(w);
+        } else {
+            w.value(std::to_string(std::get<1>(var_)));
+        }
     }
 
 private:
-    Variable var_;
+    std::variant<Variable, uint64_t> var_;
     bool is_decl_;
 };
 
@@ -189,6 +240,8 @@ public:
         instances_.emplace_back(std::make_pair(name, m));
     }
 
+    friend class SymbolTable;
+
 protected:
     void serialize_(JSONWriter &w) const override {
         w.key("name").value(name_);
@@ -199,18 +252,114 @@ protected:
         w.end_array();
         w.key("variables").begin_array();
         for (auto const &v : variables_) {
-            v.serialize(w);
+            if (v.index() == 0) {
+                std::get<0>(v).serialize(w);
+            } else {
+                w.value(std::to_string(std::get<1>(v)));
+            }
         }
         w.end_array();
     }
 
 private:
     std::string name_;
-    std::vector<Variable> variables_;
+    std::vector<std::variant<Variable, uint64_t>> variables_;
     std::vector<std::pair<std::string, const Module *>> instances_;
 };
 
 class SymbolTable {
+private:
+    // not efficient but good enough
+    class ScopeVisitor {
+    public:
+        void visit(ScopeBase *scope) {
+            if (auto *m = dynamic_cast<Module *>(scope)) {
+                for (auto const &ss : *m) {
+                    visit(ss.get());
+                }
+                handle(m);
+            } else if (auto *v = dynamic_cast<VarStmt *>(scope)) {
+                handle(v);
+            } else if (auto *s = dynamic_cast<Scope<> *>(scope)) {
+                for (auto const &ss : *s) {
+                    visit(ss.get());
+                }
+                handle(s);
+            }
+        }
+        virtual void handle(Module *) {}
+        virtual void handle(VarStmt *) {}
+        virtual void handle(Scope<> *) {}
+    };
+
+    class VariableCount : public ScopeVisitor {
+    public:
+        std::unordered_map<Variable, uint64_t, Variable::hash_fn> count;
+        void handle(Module *m) override {
+            for (auto const &v : m->variables_) {
+                if (v.index() == 0) {
+                    count[std::get<0>(v)]++;
+                }
+            }
+        }
+
+        void handle(VarStmt *s) override {
+            if (s->var_.index() == 0) {
+                count[std::get<0>(s->var_)]++;
+            }
+        }
+    };
+
+    class VarAssign : public ScopeVisitor {
+    public:
+        explicit VarAssign(const std::unordered_map<Variable, uint64_t, Variable::hash_fn> &id)
+            : id_(id) {}
+
+        void handle(Module *m) override {
+            for (auto &variable : m->variables_) {
+                auto const &v = variable;
+                if (v.index() == 0) {
+                    auto const &var = std::get<0>(v);
+                    if (id_.find(var) != id_.end()) {
+                        variable = id_.at(var);
+                    }
+                }
+            }
+        }
+
+        void handle(VarStmt *s) override {
+            if (s->var_.index() == 0) {
+                auto const &var = std::get<0>(s->var_);
+                if (id_.find(var) != id_.end()) {
+                    s->var_ = id_.at(var);
+                }
+            }
+        }
+
+    private:
+        const std::unordered_map<Variable, uint64_t, Variable::hash_fn> &id_;
+    };
+
+    class FilenameClear : public ScopeVisitor {
+    public:
+        void handle(Scope<> *scope) override { FilenameClear::handle(scope, scope->filename); }
+
+        void handle(VarStmt *stmt) override { FilenameClear::handle(stmt, stmt->filename); }
+
+    private:
+        static void handle(ScopeBase *scope, std::string &filename) {
+            if (filename.empty()) return;
+            if (!scope->parent) return;
+            // top level scope need to have filename
+            if (dynamic_cast<Module*>(scope->parent)) return;
+            auto const &ref = scope->parent->get_filename();
+            if (ref == filename) {
+                // the parent already has this filename, don't need to store it
+                filename.clear();
+            }
+        }
+    };
+
 public:
     SymbolTable(std::string framework_name, std::string top_name)
         : framework_name_(std::move(framework_name)), top_name_(std::move(top_name)) {}
@@ -227,15 +376,67 @@ public:
         for (auto const &m : modules_) {
             m->serialize(w);
         }
+        w.end_array();
 
-        w.end_array().end_obj();
+        if (!variables_.empty()) {
+            w.key("variables");
+            w.begin_array();
+            for (auto const &v : variables_) {
+                v.serialize(w);
+            }
+            w.end_array();
+        }
+
+        w.end_obj();
         return w.str();
+    }
+
+    // reduce the storage
+    void compress() {
+        // 1. compress variable referencing
+        compress_var();
+        // 2. compress filename
+        compress_filename();
     }
 
 private:
     std::string framework_name_;
     std::string top_name_;
     std::vector<std::unique_ptr<Module>> modules_;
+
+    std::vector<Variable> variables_;
+
+    void compress_var() {
+        // first pass collect all the variables
+        VariableCount v_count;
+        for (auto &m : modules_) {
+            v_count.visit(m.get());
+        }
+        // compute the global variable pull
+        variables_.reserve(v_count.count.size());
+        std::unordered_map<Variable, uint64_t, Variable::hash_fn> ids;
+        for (auto [var, c] : v_count.count) {
+            if (c > 1) {
+                auto id = variables_.size();
+                auto &var_id = variables_.emplace_back(var);
+                var_id.id = id;
+                ids.emplace(var_id, id);
+            }
+        }
+
+        // second pass to assign
+        VarAssign var_assign(ids);
+        for (auto &m : modules_) {
+            var_assign.visit(m.get());
+        }
+    }
+
+    void compress_filename() {
+        FilenameClear f;
+        for (auto &m : modules_) {
+            f.visit(m.get());
+        }
+    }
 };
 
 }  // namespace hgdb::json
--- tests/bindings/test_cxx.cc
+++ tests/bindings/test_cxx.cc
@@ -24,10 +24,9 @@ TEST(json, nested_scope) {
     SymbolTable table("test", top_name);
     auto *mod = table.add_module(top_name);
     auto *scope = mod->create_scope<Scope<>>();
-    scope->filename_ = "test.sv";
+    scope->filename = "test.sv";
     Variable var = {.name = "test", .value = "test", .rtl = true};
-    auto *s = scope->create_scope<VarStmt>(var, true);
-    s->line_num = 2;
+    auto *s = scope->create_scope<VarStmt>(var, 2, true);
 
     auto str = table.output();
     check_valid(table);
@@ -35,7 +34,7 @@ TEST(json, nested_scope) {
     // test out query as well
     hgdb::JSONSymbolTableProvider db;
     db.parse(str);
-    auto bps = db.get_breakpoints(scope->filename_, s->line_num);
+    auto bps = db.get_breakpoints(scope->filename, s->line_num);
     EXPECT_EQ(bps.size(), 1);
 }
 
@@ -60,4 +59,43 @@ TEST(json, instance) {
 
     auto vars = db.get_generator_variable(0);
     EXPECT_EQ(vars.size(), 1);
+}
+
+TEST(json, compression) {
+    using namespace hgdb::json;
+    constexpr auto top_name = "mod";
+    SymbolTable table("test", top_name);
+    auto *mod1 = table.add_module(top_name);
+    auto *scope1 = mod1->create_scope<Scope<>>();
+    auto constexpr filename = __FILE__;
+    scope1->filename = filename;
+    auto *scope2 = scope1->create_scope<Scope<>>();
+    scope2->filename = filename;
+    Variable var{.name = "a", .value = "b", .rtl = true};
+    auto constexpr line1 = __LINE__;
+    auto *stmt = scope2->create_scope<VarStmt>(var, line1, true);
+    stmt->filename = filename;
+    auto constexpr line2 = __LINE__;
+    stmt = scope2->create_scope<VarStmt>(var, line2, true);
+    stmt->filename = filename;
+    mod1->add_variable(var);
+
+    auto str = table.output();
+    check_valid(table);
+
+    table.compress();
+    str = table.output();
+    check_valid(table);
+
+    hgdb::JSONSymbolTableProvider db;
+    db.parse(str);
+    auto bps = db.get_breakpoints(__FILE__);
+    EXPECT_EQ(bps.size(), 2);
+    bps = db.get_breakpoints(filename, line1);
+    EXPECT_EQ(bps.size(), 1);
+    bps = db.get_breakpoints(filename, line2);
+    EXPECT_EQ(bps.size(), 1);
+    auto vars = db.get_context_variables(bps[0].id);
+    EXPECT_EQ(vars.size(), 1);
+    EXPECT_EQ(vars[0].first.name, var.name);
 }
