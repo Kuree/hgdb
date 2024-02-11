51f441d5b099c93ad1dfa34fe9c49c7e8dfffd00
Keyi Zhang <keyi@cs.stanford.edu>
2024-02-11 14:07:18 -0800

    [ci] adjust new pypi deployment scripts

--- bindings/python/scripts/deploy.sh
+++ bindings/python/scripts/deploy.sh
@@ -3,8 +3,8 @@ set -xe
 
 ROOT="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 ROOT="$(dirname "$(dirname "$(dirname "${ROOT}")")")"
-docker run -it --rm -d --name manylinux -v "${ROOT}":/hgdb keyiz/manylinux2010 bash
+docker run -it --rm -d --name manylinux -v "${ROOT}":/hgdb quay.io/pypa/manylinux2014_x86_64 bash
 # python changes its python tag...
-docker exec -i manylinux bash -c 'cd /hgdb/bindings/python && for PYBIN in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310; do /opt/python/${PYBIN}/bin/python setup.py bdist_wheel; done'
+docker exec -i manylinux bash -c 'cd /hgdb/bindings/python && for PYBIN in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311; do /opt/python/${PYBIN}/bin/python setup.py bdist_wheel; done'
 docker exec -i manylinux bash -c 'cd /hgdb/bindings/python&& for WHEEL in dist/*.whl; do auditwheel repair "${WHEEL}"; done'
 docker stop manylinux
